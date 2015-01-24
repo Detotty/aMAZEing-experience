1b4d8bfe8806fec11e8af936c227264416e54982
Icaro <mota.icaro@gmail.com>
2015-01-24 19:20:36 -0200

    Finished ghost

--- Assets/Scripts/GhostScript.cs
+++ Assets/Scripts/GhostScript.cs
@@ -3,25 +3,17 @@ using System.Collections;
 
 public class GhostScript : MonoBehaviour 
 {
-	public float timer = 2;
+	public float timer = 4;
+	public GameObject lantern;
 
 	private bool disappear = false;
 
 	void Update () 
 	{
-		if (disappear)
-		{
-			timer = timer - Time.deltaTime;
-		}
-		if (timer <= 0)
-			renderer.enabled = false;
-	}
-
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.GetComponent<LanternScript>() != null)
-		{
+		if (lantern.collider2D.OverlapPoint(transform.position))
 			disappear = true;
-		}
+		
+		if (disappear)
+			Object.Destroy(gameObject,timer);
 	}
 }
