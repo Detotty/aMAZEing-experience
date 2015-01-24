4dc0a418bbe27e5f5886bfc02cb179802e177b3e
Icaro <mota.icaro@gmail.com>
2015-01-24 16:43:48 -0200

    Added walking animation and fixed the lantern
    Character now shows an animation when walking in any direction.
    Lantern is now freaking PERFECT. It rotates with the character and has
    more range

--- Assets/Scripts/AnimationScript.cs
+++ Assets/Scripts/AnimationScript.cs
@@ -3,12 +3,23 @@ using System.Collections;
 
 public class AnimationScript : MonoBehaviour 
 {
+	private GameObject walking;
+
 	void Start () 
 	{
-	
+		walking = transform.Find("Walking").gameObject;
 	}
 	void Update () 
 	{
-	
+		if (rigidbody2D.velocity != new Vector2(0f,0f))
+		{
+			walking.SetActive(true);
+			renderer.enabled = false;
+		}
+		else
+		{
+			walking.SetActive(false);
+			renderer.enabled = true;
+		}
 	}
 }
