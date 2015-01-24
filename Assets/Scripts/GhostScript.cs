3363e24d0e4f204a18a0442675e8e8528add64eb
Icaro <mota.icaro@gmail.com>
2015-01-24 18:58:25 -0200

    Created the ghost
    It still doesnt disappear.

---
+++ Assets/Scripts/GhostScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostScript : MonoBehaviour 
+{
+	public float timer = 2;
+
+	private bool disappear = false;
+
+	void Update () 
+	{
+		if (disappear)
+		{
+			timer = timer - Time.deltaTime;
+		}
+		if (timer <= 0)
+			renderer.enabled = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.GetComponent<LanternScript>() != null)
+		{
+			disappear = true;
+		}
+	}
+}
