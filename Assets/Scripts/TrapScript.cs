e918c3df3da79489f602a67288fbe1323ca3ab84
Matheus Portela <matheus.v.portela@gmail.com>
2015-01-24 17:50:53 -0200

    Implementing cage trap
    This trap requires a wall to have the "CageTrapFirstWall" tag to make a
    wall vanish before the others.

--- Assets/Scripts/TrapScript.cs
+++ Assets/Scripts/TrapScript.cs
@@ -6,10 +6,18 @@ public class TrapScript : MonoBehaviour
 	public enum TrapType
 	{
 		SingleActivation,
-		ToggleActivation
+		ToggleActivation,
+		CageActivation
 	};
 
 	public TrapType trapType = TrapType.SingleActivation;
+
+	public int cageFirstDuration = 5;
+	public int cageDuration = 10;
+
+	// Variables used for timed traps
+	private bool isTrapRunning = false;
+	private float elapsedTime = 0;
 
 	void SetActiveChildren(bool value)
 	{
@@ -27,6 +35,38 @@ public class TrapScript : MonoBehaviour
 				childTransform.gameObject.SetActive(true);
 		}
 	}
+	
+	void CageActiveChildren()
+	{
+		isTrapRunning = true;
+
+		foreach (Transform childTransform in gameObject.transform)
+		{
+			childTransform.gameObject.SetActive(true);
+		}
+	}
+
+	void CageTrapFinish()
+	{
+		if (elapsedTime > cageFirstDuration && elapsedTime < cageDuration)
+		{
+			foreach (Transform childTransform in gameObject.transform)
+			{
+				if (childTransform.gameObject.CompareTag("CageTrapFirstWall"))
+					childTransform.gameObject.SetActive(false);
+			}
+		}
+		else if (elapsedTime > cageDuration)
+		{
+			foreach (Transform childTransform in gameObject.transform)
+			{
+				if (!childTransform.gameObject.CompareTag("CageTrapFirstWall"))
+					childTransform.gameObject.SetActive(false);
+			}
+
+			isTrapRunning = false;
+		}
+	}
 
 	void Awake()
 	{
@@ -35,9 +75,26 @@ public class TrapScript : MonoBehaviour
 
 	void OnTriggerEnter2D(Collider2D otherCollider)
 	{
+		elapsedTime = 0;
+
 		if (trapType == TrapType.SingleActivation)
 			SetActiveChildren(true);
 		else if (trapType == TrapType.ToggleActivation)
 			ToggleActiveChildren();
+		else if (trapType == TrapType.CageActivation)
+			CageActiveChildren();
+	}
+
+	void Update()
+	{
+		if (isTrapRunning)
+		{
+			elapsedTime += Time.deltaTime;
+
+			if (trapType == TrapType.CageActivation)
+				CageTrapFinish();
+			else
+				Debug.LogError("Trap running but no finish method. Type: " + trapType);
+		}
 	}
 }
