a6147ba5f4831b95573fcc31515afebf7d4fffde
Matheus Portela <matheus.v.portela@gmail.com>
2015-01-24 22:50:08 -0200

    Battery prefabs
    It increases lantern's power by 1, if possible.

---
+++ Assets/Scripts/BatteriesHelper.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatteriesHelper : MonoBehaviour {
+	public static BatteriesHelper Instance;
+
+	public float period = 30f;
+	
+	public Texture texture;
+	public float scale = 0.225f;
+	public float x = -5.9f;
+	public float y = -2.25f;
+
+	private float elapsedTime = 0f;
+
+	private int MAXIMUM_POWER = 3;
+
+	private int power;
+	public int Power
+	{
+		get
+		{
+			return power;
+		}
+		private set
+		{
+			if (value < 0)
+				power = 0;
+			else if (value >= MAXIMUM_POWER)
+				power = MAXIMUM_POWER;
+			else
+				power = value;
+		}
+	}
+
+	void Awake()
+	{
+		if (Instance != null)
+			Debug.LogError("More than one instance of BatteriesHelper!");
+
+		Instance = this;
+		Power = MAXIMUM_POWER;
+	}
+
+	public void DecreasePower()
+	{
+		--Power;
+		Debug.Log("Battery power: " + Power);
+		
+		if (Power == 0)
+		{
+			Debug.Log("Battery is dead");
+			GameObject playerObject = GameObject.FindWithTag("Player");
+			if (playerObject != null)
+				Destroy(playerObject);
+		}
+	}
+
+	public void IncreasePower()
+	{
+		++Power;
+		Debug.Log("Battery power: " + Power);
+	}
+
+	// Draw batteries with respect to the player position
+	void OnGUI()
+	{
+		GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+		if (playerObject != null)
+		{
+			Vector3 playerPosition = playerObject.transform.position;
+			playerPosition.x += x;
+			playerPosition.y -= y;
+			Vector3 newPosition = Camera.main.WorldToScreenPoint(playerPosition);
+			
+			Rect position = new Rect(newPosition.x, newPosition.y, scale*Screen.width*0.25f, scale*Screen.height/3f*Power);
+			Rect size = new Rect(0, 0, 1.0f, 1.0f/3f*Power);
+			GUI.DrawTextureWithTexCoords(position, texture, size);
+		}
+	}
+	
+	void Update()
+	{
+		elapsedTime += Time.deltaTime;
+		
+		if (elapsedTime > period)
+		{
+			elapsedTime = 0;
+			BatteriesHelper.Instance.DecreasePower();
+		}
+	}
+}
--- Assets/Scripts/LanternScript.cs
+++ Assets/Scripts/LanternScript.cs
@@ -1,26 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class LanternScript : MonoBehaviour 
+public class LanternScript : MonoBehaviour
 {
 	public GameObject player;
 	public float blinkingPeriod = 1.5f;
 	public float blinkingDuration = 0.2f;
-
 	private GameObject failure;
-	public float timer = 0;
-
+	public GameObject scripts;
+	private float timer = 0;
 	void Start()
 	{
 		failure = transform.Find("Failure").gameObject;
 		failure.renderer.enabled = false;
 	}
-
-	void Update() 
+	void Update()
 	{
-		transform.eulerAngles = player.transform.eulerAngles;
-		transform.position = player.transform.position;
-
+		if (player != null)
+		{
+			transform.eulerAngles = player.transform.eulerAngles;
+			transform.position = player.transform.position;
+			if (BatteriesHelper.Instance.Power == 1)
+				Blink();
+			if (BatteriesHelper.Instance.Power == 0)
+				failure.renderer.enabled = true;
+		}
+	}
+	void Blink()
+	{
 		timer = timer + Time.deltaTime;
 		if (!failure.renderer.enabled)
 		{
@@ -32,11 +38,11 @@ public class LanternScript : MonoBehaviour
 		}
 		else
 		{
-				if (timer >= blinkingDuration)
-				{
-					failure.renderer.enabled = false;
-					timer = 0;
-				}
+			if (timer >= blinkingDuration)
+			{
+				failure.renderer.enabled = false;
+				timer = 0;
+			}
 		}
 	}
 }
