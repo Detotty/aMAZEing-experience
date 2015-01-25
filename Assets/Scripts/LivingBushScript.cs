4daca74ff5a4fa46b9844116f705d1e2555248ac
Icaro <mota.icaro@gmail.com>
2015-01-25 14:36:11 -0200

    Last prefabs made
    Total of prefabs to put in the game are
    LivingBush
    Minotaur
    BatteryMouse
    Demon
    Ghost

---
+++ Assets/Scripts/LivingBushScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LivingBushScript : MonoBehaviour 
+{
+	public float delay = 10;
+	public float duration = 1;
+	private float timer = 0;
+
+	void Update () 
+	{
+		transform.renderer.enabled = true;
+	
+		timer = timer + Time.deltaTime;
+		if (timer >= delay)
+		{
+			if (timer > delay + duration)
+				timer = 0;
+			transform.renderer.enabled = false;
+		}
+	}
+}
